f7eeae32bd32a4e39cedaa4c4128cf5c9f846b7b
Christopher Homberger <christopher.homberger@web.de>
2025-07-02 12:18:51 +0200

    Add APerformanceHint symbol stubs
    * fps limiter works again

--- src/fake_swappygl.cpp
+++ src/fake_swappygl.cpp
@@ -16,4 +16,10 @@ void FakeSwappyGL::initHooks(std::vector<mcpelauncher_hook_t>& hooks) {
     hooks.emplace_back(mcpelauncher_hook_t{"SwappyGL_setWindow", (void*)+[]() -> bool { return true; }});
     hooks.emplace_back(mcpelauncher_hook_t{"SwappyGL_enableFramePacing", (void*)+[]() -> void {}});
     hooks.emplace_back(mcpelauncher_hook_t{"SwappyGL_swap", (void*)+[](EGLDisplay display, EGLSurface surface) -> bool { return fake_egl::eglSwapBuffers(display, surface); }});
+    // 1.21.90 weak symbols for APerformanceHint reporting when fps limiter is enabled
+    // TODO refactor this code and implement native performance hint reporting
+    hooks.emplace_back(mcpelauncher_hook_t{"APerformanceHint_getManager", (void*)+[]() -> void* { return nullptr; }});
+    hooks.emplace_back(mcpelauncher_hook_t{"APerformanceHint_closeSession", (void*)+[]() {  }});
+    hooks.emplace_back(mcpelauncher_hook_t{"APerformanceHint_createSession", (void*)+[]() -> void* { return nullptr; }});
+    hooks.emplace_back(mcpelauncher_hook_t{"APerformanceHint_reportActualWorkDuration", (void*)+[]() {  }});
 }
